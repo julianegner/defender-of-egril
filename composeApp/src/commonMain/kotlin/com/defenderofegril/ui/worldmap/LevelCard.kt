6a7d89d3c2bec3bec8ac5702f127043cdb1d497f
Julian Egner <j.egner@tarent.de>
2025-11-10 15:10:26 +0100

    dark mode details

--- composeApp/src/commonMain/kotlin/com/defenderofegril/ui/worldmap/LevelCard.kt
+++ composeApp/src/commonMain/kotlin/com/defenderofegril/ui/worldmap/LevelCard.kt
@@ -40,7 +40,7 @@ fun LevelCard(
     }
     
     // Text color changes based on dark mode - darker text for better readability
-    val textColor = if (isDarkMode) Color(0xFF404040) else Color.White
+    val textColor = if (isDarkMode) Color.Black else Color.White
     
     val statusText = when (worldLevel.status) {
         LevelStatus.LOCKED -> stringResource(Res.string.locked)
