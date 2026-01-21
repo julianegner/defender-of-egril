5af71930aef8c9360133f6762d82059274ccf3d7
Julian Egner <j.egner@tarent.de>
2026-01-21 17:24:34 +0100

    set location image for fortress

--- composeApp/src/commonMain/kotlin/de/egril/defender/ui/editor/worldmap/LocationIconUtils.kt
+++ composeApp/src/commonMain/kotlin/de/egril/defender/ui/editor/worldmap/LocationIconUtils.kt
@@ -21,22 +21,7 @@ object LocationIconUtils {
      * Uses icons with "location_" prefix. Falls back to "emoji_" prefix for compatibility.
      */
     val AVAILABLE_LOCATION_ICONS = listOf(
-        "location_map",           // Map icon - default/general location
-        "location_pushpin",       // Pushpin - mark a specific location
-        "location_door",          // Door - entrance/exit location
-        "location_crown",         // Crown - important/capital location
-        "location_target",        // Target - goal/objective location
-        "location_tools",         // Tools - construction/workshop location
-        "location_magnifying_glass", // Magnifying glass - discovery/search location
-        "location_money",         // Money - merchant/treasury location
-        "location_sword",         // Sword - battle/military location
-        "location_skull",         // Skull - danger/graveyard location
-        "location_test_tube",     // Test tube - alchemy/magic location
-        "location_explosion",     // Explosion - destruction/volcano location
-        "location_heart",         // Heart - sanctuary/healing location
-        "location_unlock",        // Unlock - unlocked/accessible location
-        "location_lock",          // Lock - locked/restricted location
-        "location_warning"        // Warning - hazardous location
+        "fortress"
     )
     
     /**
@@ -51,22 +36,7 @@ object LocationIconUtils {
         // Try to get the resource with the given name
         // Falls back to emoji_ prefix if location_ doesn't exist
         return when (iconResourceName) {
-            "location_map", "emoji_map" -> Res.drawable.emoji_map
-            "location_pushpin", "emoji_pushpin" -> Res.drawable.emoji_pushpin
-            "location_door", "emoji_door" -> Res.drawable.emoji_door
-            "location_crown", "emoji_crown" -> Res.drawable.emoji_crown
-            "location_target", "emoji_target" -> Res.drawable.emoji_target
-            "location_tools", "emoji_tools" -> Res.drawable.emoji_tools
-            "location_magnifying_glass", "emoji_magnifying_glass" -> Res.drawable.emoji_magnifying_glass
-            "location_money", "emoji_money" -> Res.drawable.emoji_money
-            "location_sword", "emoji_sword" -> Res.drawable.emoji_sword
-            "location_skull", "emoji_skull" -> Res.drawable.emoji_skull
-            "location_test_tube", "emoji_test_tube" -> Res.drawable.emoji_test_tube
-            "location_explosion", "emoji_explosion" -> Res.drawable.emoji_explosion
-            "location_heart", "emoji_heart" -> Res.drawable.emoji_heart
-            "location_unlock", "emoji_unlock" -> Res.drawable.emoji_unlock
-            "location_lock", "emoji_lock" -> Res.drawable.emoji_lock
-            "location_warning", "emoji_warning" -> Res.drawable.emoji_warning
+            "fortress" -> Res.drawable.fortress
             else -> null // Icon doesn't exist or not in allowed list
         }
     }
@@ -80,16 +50,9 @@ object LocationIconUtils {
     
     /**
      * Get a display name for an icon resource name.
-     * Converts resource names like "location_map" or "emoji_map" to "Map"
      */
     fun getIconDisplayName(iconResourceName: String): String {
         return iconResourceName
-            .removePrefix("location_")
-            .removePrefix("emoji_")
-            .split("_")
-            .joinToString(" ") { word ->
-                word.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
-            }
     }
     
     /**
