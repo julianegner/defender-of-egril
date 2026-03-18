a29f0f774830096058cab7186bcc0a3f0c667fc3
Julian Egner <j.egner@tarent.de>
2026-03-18 14:14:42 +0100

    add location images

--- composeApp/src/commonMain/kotlin/de/egril/defender/ui/editor/worldmap/LocationIconUtils.kt
+++ composeApp/src/commonMain/kotlin/de/egril/defender/ui/editor/worldmap/LocationIconUtils.kt
@@ -21,7 +21,17 @@ object LocationIconUtils {
      * Uses icons with "location_" prefix. Falls back to "emoji_" prefix for compatibility.
      */
     val AVAILABLE_LOCATION_ICONS = listOf(
-        "fortress"
+        "fortress",
+        "city",
+        "cross",
+        "dance",
+        "forest",
+        "prison",
+        "prison2",
+        "round_tower",
+        "square_tower",
+        "scroll",
+        "village"
     )
     
     /**
@@ -36,7 +46,28 @@ object LocationIconUtils {
         // Try to get the resource with the given name
         // Falls back to emoji_ prefix if location_ doesn't exist
         return when (iconResourceName) {
-            "fortress" -> Res.drawable.fortress
+            "location_fortress" -> Res.drawable.location_fortress
+            "location_city" -> Res.drawable.location_city
+            "location_cross" -> Res.drawable.location_cross
+            "location_dance" -> Res.drawable.location_dance
+            "location_forest" -> Res.drawable.location_forest
+            "location_prison" -> Res.drawable.location_prison
+            "location_prison2" -> Res.drawable.location_prison2
+            "location_round_tower" -> Res.drawable.location_round_tower
+            "location_square_tower" -> Res.drawable.location_square_tower
+            "location_scroll" -> Res.drawable.location_scroll
+            "location_village"-> Res.drawable.location_village
+            "fortress" -> Res.drawable.location_fortress
+            "city" -> Res.drawable.location_city
+            "cross" -> Res.drawable.location_cross
+            "dance" -> Res.drawable.location_dance
+            "forest" -> Res.drawable.location_forest
+            "prison" -> Res.drawable.location_prison
+            "prison2" -> Res.drawable.location_prison2
+            "round_tower" -> Res.drawable.location_round_tower
+            "square_tower" -> Res.drawable.location_square_tower
+            "scroll" -> Res.drawable.location_scroll
+            "village"-> Res.drawable.location_village
             else -> null // Icon doesn't exist or not in allowed list
         }
     }
